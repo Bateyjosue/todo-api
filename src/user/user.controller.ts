bf7e2b0afe065da69c02147370a77b90f31a9d93
Bateyjosue <josuebatey19@gmail.com>
2023-12-09 14:56:59 +0200

    feat: intercept the request and protect our endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,18 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
+
+  @UseGuards(AuthGuard('jwt'))
   @Get(':id')
   getUser(@Param() id) {
     return this.userService.getUser(id);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Get()
   getUsers() {
     return this.userService.getUsers();
